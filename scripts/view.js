df9e3d8ee773d5b80c405f002552671db3f76305
Ubinquitous <ubinquitous1@gmail.com>
2023-11-21 08:01:22 +0900

    fix : 표

--- scripts/view.js
+++ scripts/view.js
@@ -78,14 +78,13 @@ const INFO_BOX = {
 const MEMBERS = "members";
 
 (async () => {
-  const { data } = await getAllCheckIn();
+  const data = await getAllCheckIn();
   const tableList = element["@id"](MEMBERS);
 
   tableList.innerHTML = data.map(
     ({ dormitoryType, roomNumber, roomMates, checkInList }) => {
       const [firstMember, secondMember] = roomMates || ["-", "-"];
       const [firstCheckInInfo, secondCheckInInfo] = checkInList;
-
       const isCheckInFirstMember =
         firstMember?.id === firstCheckInInfo?.userSimpleWithNameRes?.id;
       const isCheckInSecondMember =
@@ -95,21 +94,17 @@ const MEMBERS = "members";
       <tr>
         <td>${dormitoryType}</td>
         <td>${roomNumber}</td>
-        <td class="room--members">
-          <div class="line--box">${firstMember?.name}</div>
-          <div>${secondMember?.name}</div>
+        <td>
+          <div class="S">${firstMember?.name}</div>
+          <div>${secondMember?.name || "-"}</div>
         </td>
-        <td class="check--in">
-          <div class="line--box">${isCheckInFirstMember ? "✔️" : "-"}</div>
+        <td>
+          <div class="S">${isCheckInFirstMember ? "✔️" : "-"}</div>
           <div>${isCheckInSecondMember ? "✔️" : "-"}</div>
         </td>
-        <td class="check--in">
-          <div class="line--box time">
-            ${dateFormat(firstCheckInInfo?.checkInTime)}
-          </div>
-          <div class="time">
-            ${dateFormat(secondCheckInInfo?.checkInTime)}
-          </div>
+        <td>
+          <div class="S">${dateFormat(firstCheckInInfo?.checkInTime)}</div>
+          <div>${dateFormat(secondCheckInInfo?.checkInTime)}</div>
         </td>
       </tr>
       `;
